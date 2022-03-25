b86e07f235ffdc70645fd4c5e68fb9afd1d5565b
fxspec06 <bshado@charter.net>
2022-03-25 12:42:23 -0400

    8 questions! updated with possible solutions to the 8 questions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,68 @@
+// Bryan Leasot
+// 3/25/2022
+
 // Question 3:
+// change the color of the items in the navbar
+// get the navbar
 var navbar = document.getElementsByClassName("navbar")[0];
-console.log(navbar);
+// get the left side section of the navbar
 var navleft = navbar.firstElementChild;
-console.log(navleft);
-
+// get an array of links
 var navitems = navleft.getElementsByClassName("link");
-console.log(navitems);
 
+// color them navy to contrast the yellow/gold background
 for (var i = 0; i < navitems.length; i++) {
   navitems[i].style.color = "navy";
 }
 
 // Question 4:
+// get the smalltext element
+var smallText = document.getElementById("smallText");
+// modify it
+smallText.innerHTML = "Suspendisse potenti";
+smallText.style.color = "#FFF";
 
 // Question 5:
+// adds a new card to the cards section of the page
+// accepts title, image url, and info / description
+function addNewCard(title, imgsrc, info) {
+  // get the cards container div
+  var cardsDiv = document.getElementsByClassName("cards")[0];
+  // create a new element *NOTE does not add to page
+  var newCard = document.createElement("article");
+  newCard.className = "card";
+  newCard.innerHTML +=
+    '<img src="' +
+    imgsrc +
+    '" /><section class="card-content"><h2>' +
+    title +
+    "</h2><p>" +
+    info +
+    "</p></section>";
+  // actually add the card to the page
+  cardsDiv.appendChild(newCard);
+}
+
+// add a new card per instruction
+addNewCard(
+  "Canyon Getaway",
+  "assets/canyon.jpg",
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae libero quasi blanditiis voluptates. Nobis enim ab dolorem aliquam necessitatibus eaque dolorum atque voluptatum,"
+);
+
+// uncomment below to test another card
+/*addNewCard(
+  'Forest Vacation',
+  'assets/forest.jpg',
+  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae libero quasi blanditiis voluptates. Nobis enim ab dolorem aliquam necessitatibus eaque dolorum atque voluptatum,'
+);*/
 
 // Question 6:
+// grab the first post
+
+var post = document.getElementsByClassName("post")[0];
+// add the image **NOTE: I added style="width: auto;" because the image was stretching horizontally on wide browser pages
+post.innerHTML += '<img style="width: auto;" src="assets/city1.jpg" />';
 
 // Question 7 & 8:
 
@@ -23,4 +70,39 @@ var Post = function (title, desc, link) {
   this.title = title;
   this.desc = desc;
   this.link = link;
+
+  this.postToPage = function () {
+    var postSection = document.getElementsByClassName("postSection")[0];
+    postSection.innerHTML +=
+      "<article class='post'><main class='post-content'><h2>" +
+      this.title +
+      "</h2><p>" +
+      this.desc +
+      '</p></main><img style="width: auto;" src="' +
+      this.link +
+      '" /></article>';
+  };
+
+  this.changeTitle = function (t) {
+    this.title = t;
+  };
+  this.changeDesc = function (d) {
+    this.desc = d;
+  };
+  this.changeLink = function (l) {
+    this.link = l;
+  };
 };
+
+var newPost = new Post(
+  "My Amazing Programming Experience",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis dolorum excepturi ullam reiciendis? Similique hic suscipit cum magnam a sunt voluptatum debitis iure, molestiae dignissimos consequuntur, minus ab nulla perferendis.</br></br>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente quae adipisci dignissimos consequuntur assumenda ratione fuga perspiciatis accusamus laborum ipsa totam nobis ex odio vel ipsum, saepe omnis? Deleniti, dolor. Dolorum minima natus tempora quidem pariatur, commodi fuga doloribus consectetur aspernatur explicabo fugit voluptas facilis voluptatum ad necessitatibus, dignissimos veritatis tempore ipsa mollitia recusandae corrupti sequi saepe, iste eos! Sed!",
+  "assets/city2.jpg"
+);
+
+newPost.postToPage();
+
+newPost.changeTitle("Look at this lake!");
+newPost.changeLink("assets/lake.jpg");
+
+newPost.postToPage();
